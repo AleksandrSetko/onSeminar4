b7282e222bfb9e9d522ba0db826707ea5e14de90
Setko AI <setko@msn.com>
2024-06-09 14:48:00 +0300

    Рандомное заполнение массива на n элементов и проверка элементов на кратность 7 и окончание числа на 1

--- onSeminar4/Program.cs
+++ onSeminar4/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel.DataAnnotations;
+
+Console.Write("Введите размер массива: ");
+int arraySize = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[arraySize];
+Random rand = new Random();
+
+int count = 0;
+
+for (int i = 0; i < arraySize; i++)
+{
+    array[i] = rand.Next(15, 22);
+    Console.Write(array[i] + " ");
+    if (array[i] % 7 == 0 && array[i] % 10 == 1)
+    {
+        count++;
+    }
+}
+
+Console.Write("\n" + count);
